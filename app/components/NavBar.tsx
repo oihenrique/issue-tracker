0a1f44935408f7ede5498ecfd088278d9338126d
Henrique <oi.henriquegomes@gmail.com>
2025-03-09 13:54:41 -0300

    style: active links on navbar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,11 +1,17 @@
-import Link from 'next/link'
-import React from 'react'
+'use client';
+
+import React from 'react';
+import Link from 'next/link';
+import classnames from 'classnames'
 import { IoBugSharp } from "react-icons/io5";
+import { usePathname } from 'next/navigation';
 
 const NavBar = () => {
+    const currentPath = usePathname();
+    
     const links = [
-        { label: 'Dashboard', href: '/ '},
-        { label: 'Issues', href: '/issues '}
+        { label: 'Dashboard', href: '/'},
+        { label: 'Issues', href: '/issues'}
     ]
 
   return (
@@ -15,8 +21,12 @@ const NavBar = () => {
             {links.map(link => 
                 <Link 
                     key={link.href} 
+                    className={classnames({
+                        'text-zinc-900': link.href === currentPath,
+                        'text-zinc-500': link.href !== currentPath,
+                        'hover:text-zinc-800 transition-colors': true
+                    })}
                     href={link.href}
-                    className='text-zinc-500 hover:text-zinc-800 transition-colors'
                     >{link.label}</Link>)
             }
         </ul>
