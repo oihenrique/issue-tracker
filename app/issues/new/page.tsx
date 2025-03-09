8abbe25def0f0dffafe88b8ad63c05ef276e2a78
Henrique <oi.henriquegomes@gmail.com>
2025-03-09 16:34:51 -0300

    add: custom radix font themes

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,12 +2,14 @@
 
 import { Button, TextArea, TextField } from '@radix-ui/themes'
 import React from 'react'
+import { MdOutlineTitle } from 'react-icons/md'
 
 const NewIssuePage = () => {
   return (
     <div className='max-w-xl space-y-3'>
         <TextField.Root placeholder="Add a title…">
             <TextField.Slot>
+                <MdOutlineTitle />
             </TextField.Slot>
         </TextField.Root>
         <TextArea placeholder="Add a description…" />
