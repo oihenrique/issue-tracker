979df30da56f001a1ff4179856657be7f8a928aa
Henrique <oi.henriquegomes@gmail.com>
2025-03-09 18:41:20 -0300

    add: error handling

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,22 +1,59 @@
-'use client'
+"use client";
 
-import { Button, TextField } from '@radix-ui/themes'
-import { MdOutlineTitle } from 'react-icons/md'
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import { MdOutlineTitle } from "react-icons/md";
+import { useForm, Controller } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-const NewIssuePage = () => {
-  return (
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root placeholder="Add a title…">
-            <TextField.Slot>
-                <MdOutlineTitle />
-            </TextField.Slot>
-        </TextField.Root>
-        <SimpleMDE placeholder="Add a description…" />
-        <Button>Submit new issue</Button>
-    </div>
-  )
+interface IssueForm {
+  title: string;
+  description: string;
 }
 
-export default NewIssuePage
+const NewIssuePage = () => {
+  const router = useRouter();
+  const [error, setError] = useState("");
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
+  return (
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root className="mb-3">
+          <Callout.Icon></Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError('An error occurred. Error code: ' + error.response.status);
+          }
+        })}
+      >
+        <TextField.Root placeholder="Add a title…" {...register("title")}>
+          <TextField.Slot>
+            <MdOutlineTitle />
+          </TextField.Slot>
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Add a description…" {...field} />
+          )}
+        />
+        <Button>Submit new issue</Button>
+      </form>
+    </div>
+  );
+};
+
+export default NewIssuePage;
